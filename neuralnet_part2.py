a8d0346c15ef7c22baf1312c7f37740c8c4c649e
Nagarjun Kumar <nagarjun.kumar@gmail.com>
2021-01-03 16:29:36 -0600

    Update neuralnet_part2.py

--- neuralnet_part2.py
+++ neuralnet_part2.py
@@ -1,16 +1,3 @@
-# neuralnet.py
-# ---------------
-# Licensing Information:  You are free to use or extend this projects for
-# educational purposes provided that (1) you do not distribute or publish
-# solutions, (2) you retain this notice, and (3) you provide clear
-# attribution to the University of Illinois at Urbana-Champaign
-#
-# Created by Justin Lizama (jlizama2@illinois.edu) on 10/29/2019
-"""
-This is the main entry point for MP6. You should only modify code
-within this file and neuralnet_part1 -- the unrevised staff files will be used for all other
-files and classes when code is run, so be careful to not modify anything else.
-"""
 
 import numpy as np
 import torch
